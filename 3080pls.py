ceb8d994f58dce1f156b94d79e5ba8247cce1376
rafaeldamasceno <rafaeldamasceno@me.com>
2021-06-13 00:55:49 +0100

    Changes for PS5

--- 3080pls.py
+++ 3080pls.py
@@ -1,13 +1,13 @@
 from scraper import scraper, STORES
 from threading import Thread
 
-thread_worten = Thread(target=scraper, args=(STORES.Worten, 'worten.log'), daemon=True)
-thread_fnac = Thread(target=scraper, args=(STORES.FNAC, 'fnac.log'), daemon=True)
-thread_mediamarkt = Thread(target=scraper, args=(STORES.MediaMarkt, 'mediamarkt.log'), daemon=True)
+thread_worten = Thread(target=scraper, args=(STORES.Worten, 'log/worten.log'), daemon=False)
+thread_fnac = Thread(target=scraper, args=(STORES.FNAC, 'log/fnac.log'), daemon=False)
+thread_mediamarkt = Thread(target=scraper, args=(STORES.MediaMarkt, 'log/mediamarkt.log'), daemon=False)
 
 thread_worten.start()
 thread_fnac.start()
 thread_mediamarkt.start()
 
-while input('Running. Type "q" to quit. ') != 'q':
-    pass
+# while input('Running. Type "q" to quit. ') != 'q':
+#     pass
--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM python:3.7-alpine
 
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
+RUN mkdir log
 COPY . .
 
-CMD ['python', '3080pls.py']
+CMD ["python", "3080pls.py"]
